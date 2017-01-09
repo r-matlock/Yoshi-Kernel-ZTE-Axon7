5a400ab4d0b0fcfaebc45f10c7d1cd08d32738a8
Eliminater74 <eliminater74@gmail.com>
2017-01-09 14:50:59 -0500

    drivers: cpufreq: Add nebula gov
     * Based on interactive ported from msm-4.4
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -551,6 +551,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIGHTNING)
 extern struct cpufreq_governor cpufreq_gov_lightning;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lightning)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
+extern struct cpufreq_governor cpufreq_gov_nebula;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
 #endif
 
 /*********************************************************************
