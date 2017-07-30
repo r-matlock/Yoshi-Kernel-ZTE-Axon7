kerneltoast/android_kernel_oneplus_msm8996

msm: sensor: actuator: Use longer damping time for lens parking
Use a longer damping time to eliminate the noise during lens parking.

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -910,7 +910,11 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 		}
 		a_ctrl->i2c_tbl_index = 0;
 		/* Use typical damping time delay to avoid tick sound */
+#ifdef CONFIG_MACH_MSM8996_15801
+		usleep_range(20000, 22000);
+#else
 		usleep_range(10000, 12000);
+#endif
 	}
 
 	return 0;

r-matlock/Yoshi-Kernel-ZTE-Axon7

Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>

drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -845,7 +845,11 @@ static int32_t msm_actuator_park_lens(struct msm_actuator_ctrl_t *a_ctrl)
 		}
 		a_ctrl->i2c_tbl_index = 0;
 		/* Use typical damping time delay to avoid tick sound */
+#ifdef CONFIG_LONGER_ACTUATOR
+		usleep_range(20000, 22000);
+#else
 		usleep_range(10000, 12000);
+#endif
 	}
 
 	return 0;